dae46ae2f7fed3003f0d48f50bd5f6e949aedea7
serraherfi267 <serraherfi@gmail.com>
2025-11-12 11:08:30 +0700

    Add files via upload

---
+++ Operator Keanggotaan Sherra.py
@@ -0,0 +1,26 @@
+perusahaan = 'Microsoft'
+list_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+
+# ini adalah dictionary, insyaalah akan kita pelajari 
+# di pertemuan -pertemuan yang akan datang
+
+mahasiswa = {
+    'nama': 'Sherra',
+    'asal': 'Jelutung 2, Simpang Rimba, Bangka Selatan'
+}
+print(
+    "Apakah 'c' ada di variabel perusahaan?",
+    'c' in perusahaan
+)
+print(
+    "Apakah 'c' tidak ada di variabel perusahaan?",
+    'c' not in perusahaan 
+)
+print(
+    "Apakah 'Madura' tidak ada di variabel list_pulau?",
+    'Madura' not in perusahaan
+)
+print(
+    "Apakah atribut 'nama' ada di variabel mahasiswa?",
+    'nama' in mahasiswa
+)
